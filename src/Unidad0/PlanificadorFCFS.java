cf477b944388ec0aae8f3af32fa6698592ee69a4
mbosu <mbosu@DESKTOP-JAH6V1T>
2020-11-14 16:48:59 +0100

    v1.3

--- src/Unidad0/PlanificadorFCFS.java
+++ src/Unidad0/PlanificadorFCFS.java
@@ -1,29 +1,43 @@
+package Unidad0;
 import java.util.LinkedList;
 import java.util.Queue;
+ 
 
 public class PlanificadorFCFS {
-	private static Queue<ProcesoImpresion> queue;
-	
-	private void Planificador() {
-		queue = new LinkedList<ProcesoImpresion>();
-	}
-	
-	private void get() {
-		ProcesoImpresion aux =queue.poll();
-		long inicio, fin;
-		try{
-			if(aux!=null) {
-			inicio= System.currentTimeMillis();
-			System.out.println("Proces"+ aux.getNombre());
-			System.out.println("Hora de salida" + inicio);
-			Thread.sleep(aux.getDuracion());
-			fin=System.currentTimeMillis();
-			System.out.println("Hora fin" + fin);
-			System.out.println("duracion del hilo " + aux.getDuracion() + " " + " ");
-			}
-		}catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    private static Queue<Proceso> queue;
+    
+    static{
+        queue = new LinkedList<Proceso>();
+    }
+    
+    // Método que toma un proceso del conjunto de procesos a servir.
+    public static Proceso get(){
+    	Proceso aux = queue.poll();
+        long inicio, fin;
+        if (aux!=null){
+            try {
+                inicio = System.currentTimeMillis();
+                System.out.println("Inicio del hilo " + aux.nombre + "||" + inicio);
+                Thread.sleep(aux.duracion);
+                fin = System.currentTimeMillis();
+                System.out.println("Fin del hilo " + aux.nombre + "||" + fin);
+                System.out.println("** Duraci�n del hilo (" + aux.duracion +") " + aux.nombre + " ||" + (fin-inicio));
+                
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }else{
+            System.out.println("Planificador sin procesos.");
+        }
+        return aux;
+    }
+    
+    public static void put(Proceso pi){
+        queue.add(pi);
+    }
+    
+    public static void sirve(){
+        while (get()!=null);
+    }
 }
+
--- src/Unidad0/PlanificadorSSTF.java
+++ src/Unidad0/PlanificadorSSTF.java
@@ -1,4 +1,46 @@
+package Unidad0;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-public class PlanificacionSSTF {
+public class PlanificadorSSTF {
+	public static List<Proceso> procesos;
+	
+	static {
+		procesos = new ArrayList<Proceso>();
+	}
 
+	public static Proceso get() {
+		long inicio, fin;
+		Proceso p = null;
+		
+		try{
+			p = procesos.get(0);
+			procesos.remove(p);
+			inicio = System.currentTimeMillis();
+			System.out.println("Hora de inicio del proceso " + p.nombre + ": " + inicio + "." + p.duracion + ".");
+			Thread.sleep(p.duracion);
+			fin = System.currentTimeMillis();
+			System.out.println("Hora de fin del proceso " + p.nombre + ": " + fin + ".");
+		}catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+		
+		return p;
+	}
+	
+	public static void put(Proceso p) {
+		procesos.add(p);
+	}
+	
+	public static void sirve() {
+		
+		while (procesos.size()>0) {
+			get();
+		}
+		
+		System.out.println("Planificación sin procesos.");
+	}
 }
